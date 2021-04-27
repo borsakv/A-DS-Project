b4c639c13db2ca094db0e74fff963c07786c712e
jaycowan <jaycowan3@gmail.com>
2021-04-27 19:23:14 +0100

    quick add of searchbar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,19 +10,33 @@
 //}
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.TOP_LEFT);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        Scene scene = new Scene(grid, 640, 480);
+        stage.setTitle("Vancouver Bus System");
+        Label searchLabel = new Label("Search");
+        grid.add(searchLabel, 0, 1);
+        TextField searchfield = new TextField();
+        grid.add(searchfield, 1,1);
+
         stage.setScene(scene);
         stage.show();
     }
