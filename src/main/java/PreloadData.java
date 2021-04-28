7accb8e494eba1853844ce05f20f0cbaa387cba3
Vitali Borsak <borsak.vitali@gmail.com>
2021-04-28 01:16:36 +0100

    Changed the loading indicator.

--- src/main/java/PreloadData.java
+++ src/main/java/PreloadData.java
@@ -3,6 +3,7 @@ import javafx.application.Preloader.StateChangeNotification.Type;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
@@ -18,7 +19,7 @@ public class PreloadData extends Preloader{
         VBox loading = new VBox(20);
         loading.setMaxWidth(Region.USE_PREF_SIZE);
         loading.setMaxHeight(Region.USE_PREF_SIZE);
-        loading.getChildren().add(new ProgressBar());
+        loading.getChildren().add(new ProgressIndicator());
         loading.getChildren().add(new Label("Please wait..."));
 
         BorderPane root = new BorderPane(loading);
